c0ab23239347e72cbbc076521e874e47fa75dd21
luongkam <luongkam@fabbi.io>
2019-11-10 12:40:28 +0700

    update watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+const gls = require('gulp-live-server');
+
+gulp.task('serve', function() {
+    // 1. serve with default settings
+    // var server = gls.static(); //equals to gls.static('public', 3000);
+    // server.start();
+ 
+    //2. serve at custom port
+    var server = gls.new('live-server', 8080);
+    server.start();
+ 
+    // 3. serve multi folders
+    // var server = gls.static(['dist', '.tmp']);
+    // server.start();
+ 
+    //use gulp.watch to trigger server actions(notify, start or stop)
+    gulp.watch('public/sass/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
 
 gulp.task('styles', () => {
     return gulp.src('public/sass/**/*.scss')
