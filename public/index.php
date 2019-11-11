3c1f89c0065a242cd1e0fa2f102c379c033d0066
Dương Hồng Đức <yangdungde123@gmail.com>
2019-11-11 16:11:22 +0700

    up index

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="my_page.html">my_page</a></li>
+    <li><a href="company_list.html">company_list</a></li>
+    <li><a href="follow_list.html">follow_list</a></li>
+    <li><a href="feed-list.html">feed-list</a></li>
+    <li><a href="guidance_list.html">guidance_list</a></li>
+    <li><a href="search-pc.html">search-pc</a></li>
+</ul>
