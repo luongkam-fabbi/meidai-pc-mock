5b66574ced5ef5c9be5a0e1119fe194e2a0c761e
Dương Hồng Đức <yangdungde123@gmail.com>
2019-11-11 10:35:06 +0700

    up follow list

---
+++ public/assets/js/follow_list.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.main-single-item').slick({
+        slidesToShow: 3.05,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+        arrows: false,
+        dotsClass: 'slick-dots-slide'
+    });
+});
