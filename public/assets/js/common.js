77799db6d3cec63b10512c26664c3ee83b1d7169
Dương Hồng Đức <yangdungde123@gmail.com>
2019-11-14 20:38:15 +0700

    dau kho

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     const elmScrollTop = '#up-to-top';
-
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $(elmScrollTop).fadeIn();
@@ -8,35 +7,33 @@ $(document).ready(function () {
             $(elmScrollTop).fadeOut();
         }
     });
-
     $(elmScrollTop).click(function () {
         $('html, body').animate({ scrollTop: 0 }, 800);
         return false;
     });
-});
-// Button More
-$(document).ready(function() {
-    var showChar = 100;  
+
+    // Button More
+    var showChar = 100;
     var ellipsestext = "...";
     var moretext = "続きを読む &nbsp;";
     var moreimage = "assets/images/extend.png";
     var lessimage = "assets/images/extend2.png";
     var lesstext = "隠す &nbsp;";
-    
-    $('.more-text').each(function() {
+
+    $('.more-text').each(function () {
         var content = $(this).html();
-        if(content.length > showChar) {
- 
+        if (content.length > showChar) {
+
             var c = content.substr(0, showChar);
             var h = content.substr(showChar, content.length - showChar);
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + 
-            '</span>&nbsp;&nbsp;<a href="#" class="extend morelink"><p>'+ moretext +'</p><img src='+ moreimage +'></a></span>';
+            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h +
+                '</span>&nbsp;&nbsp;<a href="#" class="extend morelink"><p>' + moretext + '</p><img src=' + moreimage + '></a></span>';
             $(this).html(html);
         }
     });
- 
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
+
+    $(".morelink").click(function () {
+        if ($(this).hasClass("less")) {
             $(this).removeClass("less");
             $(".morelink p").html(moretext);
             $(".morelink img").attr("src", moreimage);
@@ -49,4 +46,109 @@ $(document).ready(function() {
         $(this).prev().toggle();
         return false;
     });
+
+    // modal location
+    $('.cview--date').click(function () {
+        $(this).toggleClass('cview--date-focus');
+    });
+    let arrIndex = [1, 2, 3, 4, 5];
+
+    arrIndex.forEach((item, index) => {
+        $('.checkbox_location_0' + item + '').click(function () {
+            const checkBoxCheckAll = $('#checkbox_location_all_0' + item + '');
+            const checkBoxChecked = $('.checkbox_location_0' + item + ':checked');
+            const checkBoxAll = $('.checkbox_location_0' + item + '');
+            if (checkBoxChecked.length === checkBoxAll.length) {
+                checkBoxCheckAll.prop('checked', true);
+                return;
+            }
+            if (checkBoxChecked.length !== checkBoxAll.length) {
+                checkBoxCheckAll.prop('checked', false);
+            }
+        });
+        $('#checkbox_location_all_0' + item + '').click(function (event) {
+            if (this.checked) {
+                // Iterate each checkbox
+                $('.checkbox_location_0' + item + '').each(function () {
+                    this.checked = true;
+                });
+            } else {
+                $('.checkbox_location_0' + item + '').each(function () {
+                    this.checked = false;
+                });
+            }
+        });
+    });
+
+    const arrBtnOpenModal = ["#btn_open_modal_location", "#btn_open_modal_industry", "#btn_open_modal_job", "#btn_open_modal_feature"];
+    const arrModal = [
+        "#my_page_modal_location",
+        "#my_page_modal_industry",
+        "#my_page_modal_job",
+        "#my_page_modal_feature"];
+
+    arrBtnOpenModal.forEach((item, index) => {
+        function togleModal(isOpen) {
+            const val = isOpen ? 'block' : 'none'
+            $(arrModal[index]).css("display", val);
+        }
+        $(item).click(function () {
+            togleModal(true);
+        });
+
+        $('.modal_close_btn').click(function () {
+            togleModal(false);
+        });
+
+        $('.page_modal').click(function (e) {
+            if (e.target === this) {
+                togleModal(false);
+            }
+        });
+    })
+
+    const checkBoxCheckAllInit = $('#checkbox_input_field_1_all');
+    const checkBoxAllInit = $('.checkbox1');
+    if (checkBoxCheckAllInit.prop('checked')) {
+        checkBoxAllInit.each(function () {
+            $(this).prop('checked', true)
+        });
+    }
+
+    $('.checkbox1').click(function () {
+        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
+        const checkBoxChecked = $('.checkbox1:checked');
+        const checkBoxAll = $('.checkbox1');
+        if (checkBoxChecked.length === checkBoxAll.length) {
+            checkBoxCheckAll.prop('checked', true);
+            return;
+        }
+        if (checkBoxChecked.length !== checkBoxAll.length) {
+            checkBoxCheckAll.prop('checked', false);
+        }
+    });
+
+    $('#checkbox_input_field_1_all').click(function () {
+        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
+        const checkBoxAll = $('.checkbox1');
+        if (checkBoxCheckAll.prop('checked')) {
+            checkBoxAll.each(function () {
+                $(this).prop('checked', true)
+            });
+        } else {
+            checkBoxAll.each(function () {
+                $(this).prop('checked', false)
+            });
+        }
+    });
+
+    $('.option_item_btn').click(function () {
+        const currentClassName = $(this).prop('class');
+        if (!currentClassName.includes('option_item_btn_selected')) {
+            $('.option_item_btn').each(function () {
+                $(this).prop('class', currentClassName);
+            })
+            $(this).prop('class', currentClassName.concat(" option_item_btn_selected"));
+        }
+    })
 });
--- public/assets/js/company_list.js
+++ public/assets/js/company_list.js
@@ -1,87 +1,66 @@
-function closeModal() {
-    document.getElementById('my_page_modal').style.display = "none";
-    document.getElementById('my-page-modal-search').style.display = "none";
+function closeModal(){
+    document.getElementById('my_page_modal_calender').style.display="none";
 }
-
 $(document).ready(function () {
     const checkBoxCheckAllInit = $('#checkbox_input_field_1_all');
     const checkBoxAllInit = $('.checkbox1');
-    if (checkBoxCheckAllInit.prop('checked')) {
-        checkBoxAllInit.each(function () {
-            $(this).prop('checked', true)
+    if(checkBoxCheckAllInit.prop('checked')){
+        checkBoxAllInit.each(function() {
+            $(this).prop('checked',true)
         });
     }
 
-    function togleModal1(isOpen) {
-        const val = isOpen ? 'block' : 'none';
-        $('#my_page_modal').css("display", val);
+    function togleModal(isOpen){
+        const val = isOpen? 'block' : 'none'
+        $('#my_page_modal_calender').css("display",val);
     }
 
-    $('#btn_open_modal').click(function () {
-        togleModal1(true);
-    });
-
-    $('#my_modal_close_btn').click(function () {
-        togleModal1(false);
-    });
-
-    $('.page_modal').click(function (e) {
-        if (e.target === this) {
-            togleModal1(false);
-        }
-    });
-    $('.checkbox1').click(function () {
-        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
-        const checkBoxChecked = $('.checkbox1:checked');
-        const checkBoxAll = $('.checkbox1');
-        if (checkBoxChecked.length === checkBoxAll.length) {
-            checkBoxCheckAll.prop('checked', true);
-            return;
-        }
-        if (checkBoxChecked.length !== checkBoxAll.length) {
-            checkBoxCheckAll.prop('checked', false);
-        }
-    });
-    $('#checkbox_input_field_1_all').click(function () {
-        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
-        const checkBoxAll = $('.checkbox1');
-        if (checkBoxCheckAll.prop('checked')) {
-            checkBoxAll.each(function () {
-                $(this).prop('checked', true)
-            });
-        } else {
-            checkBoxAll.each(function () {
-                $(this).prop('checked', false)
-            });
-        }
-    });
-    $('.option_item_btn').click(function () {
-        const currentClassName = $(this).prop('class');
-        if (!currentClassName.includes('option_item_btn_selected')) {
-            $('.option_item_btn').each(function () {
-                $(this).prop('class', currentClassName);
-            });
-            $(this).prop('class', currentClassName.concat(" option_item_btn_selected"));
-        }
-    });
-
-    function togleModal(isOpen) {
-        const val = isOpen ? 'block' : 'none';
-        $('#my-page-modal-search').css("display", val);
-    }
-
-    $('#btn-open-modal-search').click(function () {
+    $('#btn_open_modal_calender').click(function(){
         togleModal(true);
     });
 
-    $('#my_modal_close_btn_search').click(function () {
+    $('#my_modal_close_btn_calender').click(function(){
         togleModal(false);
     });
-    $('.page_modal').click(function (e) {
-        if (e.target === this) {
+    $('.page_modal').click(function(e){
+        if ( e.target === this ){
             togleModal(false);
         }
     });
+    $('.checkbox1').click(function(){
+        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
+        const checkBoxChecked = $('.checkbox1:checked');
+        const checkBoxAll = $('.checkbox1');
+        if(checkBoxChecked.length === checkBoxAll.length){
+            checkBoxCheckAll.prop('checked',true);
+            return;
+        }
+        if(checkBoxChecked.length !== checkBoxAll.length){
+            checkBoxCheckAll.prop('checked',false);
+        }
+    });
+    $('#checkbox_input_field_1_all').click(function(){
+        const checkBoxCheckAll = $('#checkbox_input_field_1_all');
+        const checkBoxAll = $('.checkbox1');
+        if(checkBoxCheckAll.prop('checked')){
+            checkBoxAll.each(function() {
+                $(this).prop('checked',true)
+            });
+        }else{
+            checkBoxAll.each(function() {
+                $(this).prop('checked',false)
+            });
+        }
+    });
+    $('.option_item_btn').click(function(){
+        const currentClassName= $(this).prop('class');
+        if (!currentClassName.includes('option_item_btn_selected')){
+            $('.option_item_btn').each(function(){
+                $(this).prop('class',currentClassName);
+            })
+            $(this).prop('class',currentClassName.concat(" option_item_btn_selected"));
+        }
+    })
 
     const tutorialSelector = $('.tutorial-item');
 
@@ -109,5 +88,3 @@ $(document).ready(function () {
         }
     });
 });
-
-// ----------------------------  open modal search -----------------------------
