15c8af006a9a45178743f0590ab954ad873894ae
Monochrome <bakadwite@gmail.com>
2019-11-25 10:34:45 +0700

    responsive tutorial, message page

---
+++ public/assets/js/tutorial.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".tutorial-content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    arrows: false,
+    dotsClass: "slick-dots-slide",
+  });
+
+});
