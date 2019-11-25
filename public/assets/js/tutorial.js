7b5e15ee8f682dd1046d0926dff8de0847250565
Monochrome <bakadwite@gmail.com>
2019-11-25 11:33:06 +0700

    fix style silde, add link to index.js

--- public/assets/js/tutorial.js
+++ public/assets/js/tutorial.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-
   $(".tutorial-content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplaySpeed: 20000,
+    autoplay: true,
+    arrows: false,
+    dotsClass: "slick-dots-slide",
+  });
+
+  $(".tutorial2-slideee").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     speed: 500,
     autoplaySpeed: 2000,
     autoplay: true,
     arrows: false,
-    dotsClass: "slick-dots-slide",
+    dotsClass: "slick-dots-slide"
   });
-
 });
